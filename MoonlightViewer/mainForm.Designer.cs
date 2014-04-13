092be0dd4845ad7e04b465cf654b1da84705834f
NetoRpg <NTOProducoes@gmail.com>
2014-04-13 17:04:38 -0300

    1.0

---
+++ MoonlightViewer/mainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ImageViewer
+{
+    partial class MoonlightViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MoonlightViewer));
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(297, 165);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(100, 50);
+            this.pbMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbMain.TabIndex = 0;
+            this.pbMain.TabStop = false;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnListar.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnListar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListar.Location = new System.Drawing.Point(618, 390);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 1;
+            this.btnListar.Text = "&Open";
+            this.btnListar.UseVisualStyleBackColor = false;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // lblSize
+            // 
+            this.lblSize.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblSize.AutoSize = true;
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.Location = new System.Drawing.Point(324, 393);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(48, 17);
+            this.lblSize.TabIndex = 2;
+            this.lblSize.Text = "0 de 0";
+            this.lblSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.BackColor = System.Drawing.SystemColors.Menu;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Location = new System.Drawing.Point(540, 390);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel.Controls.Add(this.pbMain);
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(695, 388);
+            this.panel.TabIndex = 5;
+            // 
+            // MoonlightViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(695, 415);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.btnListar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MoonlightViewer";
+            this.Text = "Moonlight Viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MoonlightViewer_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMain;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ MoonlightViewer/mainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageViewer
+{
+    public partial class MoonlightViewer : Form
+    {
+        public MoonlightViewer()
+        {
+            InitializeComponent();
+        }
+
+        int index = 0;
+        List<string> files;
+        FolderBrowserDialog fbdialog = new FolderBrowserDialog();
+
+        Bitmap LoadBitmap(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                MemoryStream memoryStream = new MemoryStream(reader.ReadBytes((int)stream.Length));
+                return new Bitmap(memoryStream);
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbdialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.files = new List<string>(Directory.GetFiles(fbdialog.SelectedPath, "*.jpg"));
+                pbMain.Image = null;
+                lblSize.Text = "0 de 0";
+                this.index = 0;
+                changePicture();
+            } 
+        }
+
+
+        private void changePicture(Keys key = Keys.Escape) {
+
+            if (this.files == null || this.files.Count <= 0)
+                return;
+
+            switch(key){
+                case Keys.Right:
+                    if (this.index < this.files.Count -1)
+                        this.index++;
+                    break;
+                case Keys.Left:
+                    if(this.index > 0)
+                        this.index--;
+                    break;
+            }
+            
+
+            Bitmap bitmap = LoadBitmap(files[this.index]);
+
+            Size size = bitmap.Size;
+
+            if (size.Width > this.panel.Width)
+            {
+                int height = (int)((double)(this.panel.Width) / ((double)bitmap.Width / (double)bitmap.Height));
+                size = new Size((this.panel.Width), height);
+            }
+
+            if (size.Height > this.panel.Height)
+            {
+                int width = (int)((double)(this.panel.Height) * ((double)bitmap.Width / (double)bitmap.Height));
+                size = new Size(width, (this.panel.Height));
+            }
+
+            if(size != bitmap.Size)
+                bitmap = new Bitmap(bitmap, size);
+
+            pbMain.Size = bitmap.Size;
+
+            pbMain.Image = bitmap;
+
+            pbMain.Location = new Point(
+                this.panel.Width / 2 - pbMain.Size.Width / 2,
+                this.panel.Height / 2 - pbMain.Size.Height / 2
+            );
+
+            this.Text = String.Format("Moonlight Viewer - {0}",Path.GetFileName (files[index]));
+            this.lblSize.Text = String.Format("{0} de {1}", this.index + 1, files.Count);
+        }
+
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == Keys.Left || keyData == Keys.Right)
+            {
+                changePicture(keyData);
+                return true;
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            changePicture();
+        }
+
+        private void MoonlightViewer_Load(object sender, EventArgs e)
+        {
+            lblSize.Text = String.Empty;
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (this.files == null || this.files.Count <= 0)
+                return;
+
+           string path = Path.Combine(fbdialog.SelectedPath, "Removed");
+
+           if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            Directory.Move(files[index], Path.Combine(path, Path.GetFileName(files[index])));
+
+            this.files.RemoveAt(this.index);
+            changePicture();
+        }
+
+
+
+    }
+}
